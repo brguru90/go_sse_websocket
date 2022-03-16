49c7f8e16c8ce1784bd930d9b6478c699d9fd4e3
Guruprasad BR <brguru90@gmail.com>
2022-03-16 13:59:04 +0530

    .

--- backend/src/ws/views/test_ws_api.go
+++ backend/src/ws/views/test_ws_api.go
@@ -8,13 +8,19 @@ import (
 	"gopkg.in/olahol/melody.v1"
 )
 
-
-func TestWS(c *gin.Context,w ws_modules.WsHandlers) {	
+func TestWS(c *gin.Context, w ws_modules.WsHandlers) {
 	fmt.Println("TestWS")
 	w.M.HandleRequest(c.Writer, c.Request)
 }
 
-func  TestWSH(s *melody.Session, msg []byte,w ws_modules.WsHandlers) {
-	fmt.Println("TestWSH",msg)
-	w.M.Broadcast(msg)
+func TestWSH(s *melody.Session, msg []byte, w ws_modules.WsHandlers) {
+	switch string(msg) {
+	case "hi":
+		w.M.Broadcast([]byte("bi"))
+	case "GM":
+		w.M.Broadcast([]byte("GN"))
+	default:
+		w.M.Broadcast(msg)
+	}
+
 }
