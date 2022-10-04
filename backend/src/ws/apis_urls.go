8fe19cb66e014b1135be097407341ea03c45a771
Guruprasad BR <brguru90@gmail.com>
2022-10-05 02:29:33 +0530

    :(

--- backend/src/ws/apis_urls.go
+++ backend/src/ws/apis_urls.go
@@ -2,11 +2,10 @@ package ws
 
 import (
 	"learn_go/src/ws/views"
-	"learn_go/src/ws/ws_modules"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitWS(router *gin.RouterGroup) {
-	router.GET(ws_modules.GetWsHandlers("chat/",views.TestWSH))
+	router.GET("chat/", views.TestWSH)
 }
--- backend/src/ws/views/test_ws_api.go
+++ backend/src/ws/views/test_ws_api.go
@@ -2,32 +2,32 @@ package views
 
 import (
 	"fmt"
-	"learn_go/src/ws/ws_modules"
 
+	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
 
-func TestWSH(w *ws_modules.WsHandlers) {
-
-	w.OnConnect = func(s *melody.Session) {
-		fmt.Println("on Connect")
-		w.M.Broadcast([]byte("connection success : initial message from server"))
-	}
-
-	w.OnMessage = func(s *melody.Session, msg []byte) {
+func TestWSH(c *gin.Context) {
+	M := melody.New()
+	M.HandleMessage(func(s *melody.Session, msg []byte) {
 		fmt.Println("on Receiving message")
 		switch string(msg) {
 		case "hi":
-			w.M.Broadcast([]byte("bi"))
+			M.Broadcast([]byte("bi"))
 		case "GM":
-			w.M.Broadcast([]byte("GN"))
+			M.Broadcast([]byte("GN"))
 		default:
-			w.M.Broadcast(msg)
+			M.Broadcast(msg)
 		}
-	}
+	})
+	M.HandleConnect(func(s *melody.Session) {
+		fmt.Println("on Connect")
+		M.Broadcast([]byte("connection success : initial message from server"))
 
-	w.OnDisconnect=func(s *melody.Session) {
+	})
+	M.HandleDisconnect(func(s *melody.Session) {
 		fmt.Println("on Disconnect")
-	}
+	})
 
+	M.HandleRequest(c.Writer, c.Request)
 }
