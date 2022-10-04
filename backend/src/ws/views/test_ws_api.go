10cc8af8a75edeb911de61a2a1028c2777c0b35b
Guruprasad BR <brguru90@gmail.com>
2022-10-04 13:15:07 +0530

    .

--- backend/src/ws/views/test_ws_api.go
+++ backend/src/ws/views/test_ws_api.go
@@ -7,27 +7,6 @@ import (
 	"gopkg.in/olahol/melody.v1"
 )
 
-// func TestWSH(s *melody.Session, msg []byte, w ws_modules.WsHandlers,status int) {
-// 	if msg==nil && status!=1{
-// 		if status==0{
-// 			fmt.Println("on Connect")
-// 			w.M.Broadcast([]byte("connection success : initial message from server"))
-// 		} else if status==2{
-// 			fmt.Println("on Disconnect")
-// 		}
-// 		return
-// 	}
-// 	fmt.Println("on Receiving message")
-// 	switch string(msg) {
-// 	case "hi":
-// 		w.M.Broadcast([]byte("bi"))
-// 	case "GM":
-// 		w.M.Broadcast([]byte("GN"))
-// 	default:
-// 		w.M.Broadcast(msg)
-// 	}
-// }
-
 func TestWSH(w *ws_modules.WsHandlers) {
 
 	w.OnConnect = func(s *melody.Session) {
